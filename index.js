4a2d2f39128833be3c224b07871ca12a4929c720
Matheus Alves Albuquerque <mathalbuquerque@MacBook-Pro-de-Matheus.local>
2020-02-22 12:40:17 -0300

    finalizando challenge 1

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ server.use(express.json());
 
 const projects = []
 
+
+function checkIdprojects(req,res,next){
+  const {id} = req.params;
+  project=projects.find(p=>p.id==id);
+    if (!project) {
+      return res.status(400).json({ error: 'Project not found' });
+    }
+  return next()
+}
+function countRequest(req,res,next){
+  console.count('Número de requisições');
+  return next();
+}
+server.use(countRequest)
+
 server.get('/projects',(req,res)=>{
   res.json(projects)
 });
@@ -23,30 +38,33 @@ server.post('/projects',(req,res)=>{
   res.status(200).json(projects)
 }); 
 
-server.put('/projects/:id',(req,res)=>{
-  const id = req.params;
-  const {title} = req.body;  
-  const project = projects.find(pro => pro.id == id);
+server.put('/projects/:id',checkIdprojects, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(pro=> pro.id == id);
 
   project.title = title;
 
-  return res.json(project)
-})
+  return res.json(project);
+});
 
-server.delete('/projects/:id',(req,res)=>{
+server.delete('/projects/:id',checkIdprojects,(req,res)=>{
   const id = req.params;
   const project = projects.find(project => project.id == id);
   projects.splice(project,1)
 
   return res.send('deletado com sucesso')
 })
-server.post('/projects/:id/tasks',(req,res)=>{
-  const id = req.params;
+server.post('/projects/:id/tasks',checkIdprojects,(req,res)=>{
+  const {id} = req.params;
   const {title}  = req.body;
-  const project = projects.find(project => project.id == id);
-  project.tasks=title
+  const project = projects.find(pro => pro.id == id);
+ 
+  
+  project.tasks.push(title)
 
-  res.status(200).json(project  )
+  res.status(200).json(project)
 }); 
 
 
